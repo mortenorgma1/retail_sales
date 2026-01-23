c4ca9b6e114693a3b499688a38bada192bcdd1b5
Morten Orgma <mortenorgma1@gmail.com>
2026-01-23 12:55:44 +0100

    Update 02_top_products_by_revenue.sql

--- sql/02_top_products_by_revenue.sql
+++ sql/02_top_products_by_revenue.sql
@@ -1,3 +1,7 @@
+/*
+Top products by revenue: ranks products based on total revenue generated and returns the highest earning items (optionally per month/category).
+*/
+
 select 
 p.product_name,
 round(sum(oi.quantity * p.price),2) as total_revenue
@@ -6,3 +10,4 @@ join products p
 on oi.product_id = p.product_id
 group by p.product_name
 order by total_revenue desc
+
